8121425c47b27b14ee3a544ebc01ac2a694ece24
shuxin.he <shuxin.he@socialcredits.cn>
2022-09-13 09:59:43 +0800

    feat: update pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,4 +1,4 @@
-//
+// Transfer a byte between two process throw pipe;
 // Created by suxin on 2022/6/13.
 //
 
@@ -13,29 +13,37 @@ void panic(char *s) {
   exit(1);
 }
 
-void pingpong(int infd, int outfd, char processCode) {
+void pingpong(int read_fd, int write_fd, char processCode) {
   char c;
   int count = 0;
-  if (processCode == 'A') write(outfd, ball, 1);
+  if (processCode == 'A') write(write_fd, ball, 1); // start then ping-pong game
 
-  while(read(infd, &c, 1) > 0) {
+  // 循环从 read_fd 读取一个字符到变量 c。
+  while(read(read_fd, &c, 1) > 0) {
+    fprintf(1, "round: %d\n", count);
     if(processCode == 'A') fprintf(1, "A to B\n");
     else fprintf(1, "B to A\n");
     if(++count > 1000) break;
-    write(outfd, ball, 1);
+    write(write_fd, ball, 1);
   }
 
-  close(infd);
-  close(outfd);
+  close(read_fd);
+  close(write_fd);
   exit(0);
 }
 
 int main(int argc, char *argv[]) {
+  // save pipe file descriptor
   int p1[2];
   int p2[2];
+
   int starttime;
   int endtime;
 
+  // pipe 创建一个小内核缓存，并暴露给进程使用，允许两个进程之间相互通讯。
+  // pipe[0] 为读取端，pipe[1] 为写入端，读取和写入端完全没有进程引用时会被关闭。
+  // 例：close(pipe[0]) 释放当前进程 pipe[0] 的引用。
+  // read 会阻断程序运行直到 不可能再读取数据，比如 pipe[1](write) 文件描述器不在被任何进程引用。
   pipe(p1);
   pipe(p2);
 
@@ -49,11 +57,12 @@ int main(int argc, char *argv[]) {
 
   // child B
   if(fork() == 0) {
-    close(p2[1]);
     close(p1[0]);
+    close(p2[1]);
     pingpong(p2[0], p1[1], 'B');
   }
 
+  // 主进程中释放所有 创建的 文件描述器
   close(p1[0]);
   close(p1[1]);
   close(p2[0]);
